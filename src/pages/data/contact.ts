d2ae4bea3a02822f9ba461b4e8f121e2c7ebd424
GSak29 <gsak0298@gmail.com>
2025-12-25 23:20:54 +0530

    test

--- src/pages/data/contact.ts
+++ src/pages/data/contact.ts
@@ -5,7 +5,7 @@ export const contactData = {
     phone: "Phone Number: 6374425918",
     mailtoEmail: "gurusakthis.29aug05@gmail.com",
     frontTitle: "Email",
-    frontDescription: "Click the flip icon in the card's corner to flip the cards  <br/> Click the icons for direct navigations",
+    frontDescription: "Click the flip icon in the card's corner to flip the cards  <br/> Click the floating logos for direct navigations",
     backDescription: "Contact for collaborations!",
     codingProfiles: [
         { name: "LeetCode", url: "https://leetcode.com/u/Guru-Sakthi/", icon: leetcodeImg }, // Add your ID
